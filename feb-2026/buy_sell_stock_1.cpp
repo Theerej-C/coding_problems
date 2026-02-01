72e68d03e096a409aa8501748ae38092630f2ca7
Theerej-C <theerej@gmail.com>
2026-02-01 20:29:41 +0530

    01-02-2026

---
+++ feb-2026/buy_sell_stock_1.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+  int maxProfit(vector<int> &prices) {
+    int mi = INT_MAX;
+    int ma = 0;
+    for (int i = 0; i < prices.size(); i++) {
+      if (prices[i] < mi) {
+        mi = prices[i];
+      }
+      ma = max(prices[i] - mi, ma);
+    }
+    return ma;
+  }
+};
